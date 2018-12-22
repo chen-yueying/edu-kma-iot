9dbbafa4cccfd39006b3201ccd7fcc117acd312e
tranvietanh190196 <tranvietanh190196@gmail.com>
2018-12-22 13:23:33 +0700

    22/12/2018

--- spring-kma-iot/src/main/java/edu/kma/iot/dao/impl/TemperatureDAOimpl.java
+++ spring-kma-iot/src/main/java/edu/kma/iot/dao/impl/TemperatureDAOimpl.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -27,33 +28,58 @@ public class TemperatureDAOimpl implements DeviceDAO{
 	public void setSessionFatory(LocalSessionFactoryBean sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
+	
 	@Override
+	@SuppressWarnings("rawtypes")
 	public void insert(Device device) {
 		SensorTemperature sensor = (SensorTemperature) device;
 		Session session = sessionFactory.getObject().openSession();
-		session.save(sensor);
-		session.beginTransaction().commit();
+		Transaction tran = session.beginTransaction();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("ss:mm:hh dd/MM/yyy");
+		String sql = " insert into SENSOR_TEMPERATURE (mac_address, status_time, temperature_value, moisture_value)"
+				+ " values (:mac, :time, :temp, :mois) ";
+		Query query = session.createNativeQuery(sql);
+		query.setParameter("mac", sensor.getMac_address());
+		query.setParameter("temp", sensor.getTemperature_value());
+		query.setParameter("mois", sensor.getMoisture_value());
+		query.setParameter("time", dateFormat.format(new Date()));
+		query.executeUpdate();
+		tran.commit();
 		session.close();
 		LOG.info("Insert SensorTemperator " + sensor.getMac_address() + " done!");
 	}
-
+	
 	@Override
+	@SuppressWarnings("rawtypes")
 	public SensorTemperature get(String mac_address) {
 		try (Session session = sessionFactory.getObject().openSession()) {
+			Transaction tran = session.beginTransaction();
 			SensorTemperature temperature = session.get(SensorTemperature.class, mac_address);
+			if(temperature == null) {
+				String sql = "insert into SENSOR_TEMPERATURE (mac_address) values (:mac)";
+				Query query = session.createNativeQuery(sql);
+				query.setParameter("mac", mac_address);
+				query.executeUpdate();
+				temperature = session.get(SensorTemperature.class, mac_address);
+				session.flush();
+				tran.commit();
+				LOG.info("Insert SensorTemperator " + mac_address + " done!");
+			}
 			return  temperature;
 		}
 	}
 
 	@Override
 	public void update(Device device) {
-		SensorTemperature sensor = (SensorTemperature) device;
+		SensorTemperature sen = (SensorTemperature) device;
 		Session session = sessionFactory.getObject().openSession();
 		Transaction tran = session.beginTransaction();
 		SimpleDateFormat dateFormat = new SimpleDateFormat("ss:mm:hh dd/MM/yyy");
-		SensorTemperature sen = (SensorTemperature) session.merge(sensor);
+		SensorTemperature sensor = (SensorTemperature) session.merge(sen);
+		sensor.setTemperature_value(sen.getTemperature_value());
+		sensor.setMoisture_value(sen.getMoisture_value());
 		sen.setStatus_time(dateFormat.format(new Date()));
-		session.save(sen);
+		session.save(sensor);
 		tran.commit();
 		session.close();
 		LOG.info("update SensorTemperature " + sensor.getMac_address() + " done!");
